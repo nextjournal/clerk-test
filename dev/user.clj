080841a49f967b8c976de78e9d697f67a9377f41
Andrea Amantini <lo.zampino@gmail.com>
2022-10-24 12:01:32 +0200

    Proposed exec-fn in user ns

--- dev/user.clj
+++ dev/user.clj
@@ -1,7 +1,13 @@
 (ns user
-  (:require [nextjournal.clerk :as clerk]
-            [nextjournal.clerk.kaocha :as clerk.kaocha]
-            [kaocha.repl]))
+  (:require [kaocha.repl]
+            [kaocha.runner]
+            [nextjournal.clerk :as clerk]
+            [nextjournal.clerk.kaocha :as clerk.kaocha]))
+
+(defn exec-fn [m]
+  (clerk/serve! {:port 8888 :browse? true})
+  (clerk/show! 'nextjournal.clerk.kaocha)
+  (kaocha.runner/exec-fn {:reporter [clerk.kaocha/report]}))
 
 (comment
   (clerk/serve! {})
